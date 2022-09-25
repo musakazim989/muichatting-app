e54bed4b793e644253a0a7edd388730b927f7432
musakazim989 <musakazim989@gmail.com>
2022-09-25 22:46:58 +0600

    show friend initializing

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,129 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { getDatabase, ref, set } from "firebase/database"
 
 const Friends = () => {
+  const db = getDatabase()
+  useEffect(() => {
+    set(ref(db, "friends/"), {
+      username: name,
+      email: email,
+    })
+  }, [])
+
   return (
     <div className="grouplist friendlist">
       <h2>Friends</h2>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-
-      <div className="divider"></div>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
-      <div className="box">
-        <div className="img">
-          <img src="./assets/images/group.jpg" alt="" />
-        </div>
-        <div className="name">
-          <h4>Fishing 2020</h4>
-          <h5>The best fishing Group</h5>
-        </div>
-        <div className="button">
-          <p>Today, 8:56pm</p>
-        </div>
-      </div>
-      <div className="divider"></div>
       <div className="box">
         <div className="img">
           <img src="./assets/images/group.jpg" alt="" />
