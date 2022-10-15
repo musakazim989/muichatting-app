95c568abf8ba850da3bdd00422b2668edf81d7b7
musakazim989 <musakazim989@gmail.com>
2022-10-16 00:14:43 +0600

    password reset done

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,7 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
+import { TextField, Button } from "@mui/material"
+import { getAuth, sendPasswordResetEmail } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
 const ResetPassword = () => {
-  return <div className="forgotpassword">high</div>
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState()
+
+  let handlePasswordReset = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        navigate("/login")
+
+        console.log("email send")
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+
+        console.log(errorCode)
+        // ..
+      })
+  }
+  return (
+    <div className="forgotpassword">
+      <div className="box">
+        <h2>Reset Passowrd</h2>
+        <div className="forgot">
+          <h2>Forgot Password</h2>
+          <p>Enter your registered email.</p>
+          <TextField
+            id="outlined-basic"
+            label="Enter Email"
+            variant="outlined"
+            style={{ width: "100%" }}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Button
+            onClick={handlePasswordReset}
+            style={{ width: "100%", marginTop: "30px" }}
+            variant="contained"
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ResetPassword
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,7 +11,6 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   FacebookAuthProvider,
-  sendPasswordResetEmail,
 } from "firebase/auth"
 
 const Login = () => {
@@ -120,19 +119,6 @@ const Login = () => {
       })
   }
 
-  let handleFrogotPassword = () => {
-    sendPasswordResetEmail(auth, email)
-      .then(() => {
-        // Password reset email sent!
-        // ..
-      })
-      .catch((error) => {
-        const errorCode = error.code
-        const errorMessage = error.message
-        // ..
-      })
-  }
-
   return (
     <section className="registraion-part login-part">
       <Grid container spacing={2}>
@@ -218,11 +204,7 @@ const Login = () => {
                 Don't have an account? <Link to="/"> Sign Up </Link> here.
               </span>
               <br />
-              <p
-                onClick={handleFrogotPassword}
-                className="form-bottom-text p"
-                style={{ marginTop: "5px" }}
-              >
+              <p className="form-bottom-text p" style={{ marginTop: "5px" }}>
                 Froget password? <Link to="/resetpassword">Click here.</Link>
               </p>
             </div>
